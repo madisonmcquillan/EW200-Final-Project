ea86dcb8f16cb54f93179d00c319e0e22729ab08
madisonmcquillan <m264212@usna.edu>
2023-11-15 09:13:35 -0500

    created sprites

--- overworld.py
+++ overworld.py
@@ -1,13 +1,31 @@
 import pygame
 from game_data import levels
 
+
+class Node(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__()
+        self.image = pygame.Surface((80, 64))
+        self.image.fill('red')
+        self.rect = self.image.get_rect(center=pos)
+
+
 class Overworld:
     def __init__(self, start_level, max_level, surface):
-
         # setup
         self.display_surface = surface
         self.max_level = max_level
         self.current_level = start_level
 
+        # sprites
+        self.setup_nodes()
+
+    def setup_nodes(self):
+        self.nodes = pygame.sprite.Group()
+
+        for node_data in levels.values():
+            node_sprite = Node(node_data['node_pos'])
+            self.nodes.add(node_sprite)
+
     def run(self):
-        pass
+        self.nodes.draw(self.display_surface)
